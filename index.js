c8c9972238378438a7ed474328fa34438e114d53
Manish Rawal <rawalsinghmanish@gmail.com>
2021-06-27 18:26:23 +0545

    built-in-validators-in-mongoose-like-required-min-max-enum-implemented

--- index.js
+++ index.js
@@ -6,24 +6,44 @@ mongoose
   .catch((err) => console.error("could not connect to MongoDB..", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true, minlength: 5, maxlength: 255 },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+  },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
+
 async function createCourse() {
   const course = new Course({
     name: "Angular Course",
+    category: "-",
     author: "Mosh",
     tags: ["angular", "frontend"],
     isPublished: true,
+    price: 1,
   });
 
-  const result = await course.save();
-  console.log(result);
+  try {
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 }
 
 async function getCourses() {
@@ -38,4 +58,4 @@ async function getCourses() {
   console.log(courses);
 }
 
-getCourses();
+createCourse();
